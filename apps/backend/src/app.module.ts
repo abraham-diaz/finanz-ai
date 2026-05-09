992b2561eae8e5d9921d65bd76b3fb1374dd731a
Abraham <202311503+abraham-diaz@users.noreply.github.com>
2026-05-09 11:39:24 +0200

    feat: restructure backend modules and implement category and transaction features

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { CategoryModule } from './modules/category/category.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, CategoryModule, TransactionModule],
 })
 export class AppModule {}
