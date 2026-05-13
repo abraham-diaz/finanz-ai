32ad258492411c884d1fac9803d5905436e7a4a7
Abraham <202311503+abraham-diaz@users.noreply.github.com>
2026-05-13 16:10:48 +0200

    feat: implement Auth module with controller, service, and DTOs; add class-validator dependency

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { CategoryModule } from './modules/category/category.module';
 import { TransactionModule } from './modules/transaction/transaction.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, CategoryModule, TransactionModule],
+  imports: [UsersModule, CategoryModule, TransactionModule, AuthModule],
 })
 export class AppModule {}
